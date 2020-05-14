22fbadf7137d79667a2bdfb4ab1165e2e471d326
JustasMit <segjustas12@gmail.com>
2020-05-14 20:29:43 +0300

    .

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -4,10 +4,6 @@
       header="To do"
       lead="Yet another 'to do' application made with Vue"
     >
-      <hr />
-      <b-button variant="success" href="https://github.com/JustasMit/vueApp"
-        >GitHub</b-button
-      >
     </b-jumbotron>
   </div>
 </template>
